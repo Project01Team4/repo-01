2cbf28bd21b763910c9fb1398c0c1f31b4547b14
Andrew <ATHur1104@gmail.com>
2023-05-14 14:15:22 -0500

    Added placeholder for name input

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -34,12 +34,12 @@ fetch(apiDndMon)
             .then(response => response.json())
             .then(data => {
                 var placeHolderImage = "https://i.kym-cdn.com/entries/icons/original/000/019/740/spoons.jpg";
-// monsterImageUrl is the url for the monster image
-// this will become background image
+                // monsterImageUrl is the url for the monster image
+                // this will become background image
                 var monsterImageUrl = data.image ? baseUrl + data.image : placeHolderImage;
                 console.log(monsterImageUrl);
                 console.log(data);
-// fightMon is the object that contains monster information
+                // fightMon is the object that contains monster information
                 var fightMon = {
                     name: data.name,
                     health: data.hit_points,
@@ -48,7 +48,7 @@ fetch(apiDndMon)
                 console.log(fightMon)
             })
     });
-    
+
 
 var dialogueIteration = 0;
 
@@ -64,10 +64,10 @@ var initialSelect = document.getElementById("initial-select");
 
 
 // generates initial start screen using start button event listener
-function startPage(){
+function startPage() {
     initialSelect.innerHTML = '';
     startBtn.textContent = ">START!";
-    startBtn.addEventListener("click", function(){
+    startBtn.addEventListener("click", function () {
         beginAdventure()
     });
 };
@@ -76,19 +76,19 @@ startPage();
 
 
 // prompts user to either go back to start or enter
-function beginAdventure(){
+function beginAdventure() {
     initialSelect.innerHTML = '';
     startBtn.style.display = "none";
     var goBack = document.createElement("button");
     goBack.textContent = ">Return to start";
     goBack.style.marginRight = "70px"
-    goBack.addEventListener("click", function(){
+    goBack.addEventListener("click", function () {
         location.reload();
     });
 
     var goAdventure = document.createElement("button");
     goAdventure.textContent = ">I'm ready to go!";
-    goAdventure.addEventListener("click", function(){
+    goAdventure.addEventListener("click", function () {
         setName();
     });
 
@@ -100,74 +100,98 @@ function beginAdventure(){
 };
 
 // allows user to set name
-function setName(){
+function setName() {
     initialSelect.innerHTML = '';
     var enterName = document.createElement("input", "text");
     enterName.textContent = "Enter your name";
+    enterName.setAttribute("placeholder", "Enter Your Name")
     var submitName = document.createElement("button");
     submitName.textContent = ">Submit!"
-    submitName.addEventListener("click", function(){
+    submitName.addEventListener("click", function () {
         var nameVal = enterName.value;
         localStorage.setItem('player-name', JSON.stringify(nameVal));
         playernameArray.push(nameVal);
 
-        if (nameVal === undefined || nameVal === ''){
+        if (nameVal === undefined || nameVal === '') {
             alert("Come now, every adventurer has a name. Make one up if you have to!")
         } else {
-            gameIntro()};
+            gameIntro()
+        };
     });
     initialSelect.append(enterName, submitName);
     return;
 };
 
-function gameIntro(){
+function gameIntro() {
     var playerName = playernameArray.join('');
     var introDialogue = [
-        {grogg: "Grogg: Hey, careful now! Stand up slowly.",
-        player: ">[STAND UP]"},
-        {grogg: "Grogg: There you go. We wouldn't want you fainting and drowning, would we?",
-        player: ">Who are you?"},
-        {grogg: "Grogg: Oh? Me? Why, I'm Grogg, of course! Soggy Grogg, to be exact, but you can call me Grogg. All my friends do. Lovely to meet you, " + playerName.toUpperCase() + ".",
-        player: ">How do you know my name?"},
-        {grogg: "Grogg: I know that and a great deal of other things about you, young " + playerName.toUpperCase() + ". A witch of my years knows many things. Anyhow, I've just saved your life!",
-        player: ">Huh? I almost died?"},
-        {grogg: "Grogg: I found you floating face-down in my bog. Another minute and you would've been fertilizing my peat. But you're alive! All thanks to me. So now, I believe you owe me a debt.",
-        player: ">Are you going to eat me?"},
-        {grogg: "Grogg: What? Eat you? How could you possibly repay me if I ate you? Goodness, I know you're barely alive, but clean the mud from your ears and try to listen.",
-        player: ">Okay..."},
-        {grogg: "Grogg: No, young " + playerName.toUpperCase() + ", I need a different type of assistance.",
-        player: ">Ominous. Go on."},
-        {grogg: "Grogg: You see, my beloved bog has been overrun! Overtaken! Scourged by beasts and monsters! I would take care of them myself, but I'm afraid my strength isn't what it used to be...",
-        player: ">I see. And this is where I come in?"},
-        {grogg: "Grogg: Exactly! You learn quickly. In order to repay me for saving you, you're going to clear them out for me. Sound good?",
-        player: ">Do I have much of a choice in the matter?"},
-        {grogg: "Grogg: Well...'choice' is a subjective term. After each monster killed, you can choose to keep adventuring or choose to return and rest at my hut.",
-        player: ">But I can't leave. Is that right?"},
-        {grogg: '',
-        player: ''}
+        {
+            grogg: "Grogg: Hey, careful now! Stand up slowly.",
+            player: ">[STAND UP]"
+        },
+        {
+            grogg: "Grogg: There you go. We wouldn't want you fainting and drowning, would we?",
+            player: ">Who are you?"
+        },
+        {
+            grogg: "Grogg: Oh? Me? Why, I'm Grogg, of course! Soggy Grogg, to be exact, but you can call me Grogg. All my friends do. Lovely to meet you, " + playerName.toUpperCase() + ".",
+            player: ">How do you know my name?"
+        },
+        {
+            grogg: "Grogg: I know that and a great deal of other things about you, young " + playerName.toUpperCase() + ". A witch of my years knows many things. Anyhow, I've just saved your life!",
+            player: ">Huh? I almost died?"
+        },
+        {
+            grogg: "Grogg: I found you floating face-down in my bog. Another minute and you would've been fertilizing my peat. But you're alive! All thanks to me. So now, I believe you owe me a debt.",
+            player: ">Are you going to eat me?"
+        },
+        {
+            grogg: "Grogg: What? Eat you? How could you possibly repay me if I ate you? Goodness, I know you're barely alive, but clean the mud from your ears and try to listen.",
+            player: ">Okay..."
+        },
+        {
+            grogg: "Grogg: No, young " + playerName.toUpperCase() + ", I need a different type of assistance.",
+            player: ">Ominous. Go on."
+        },
+        {
+            grogg: "Grogg: You see, my beloved bog has been overrun! Overtaken! Scourged by beasts and monsters! I would take care of them myself, but I'm afraid my strength isn't what it used to be...",
+            player: ">I see. And this is where I come in?"
+        },
+        {
+            grogg: "Grogg: Exactly! You learn quickly. In order to repay me for saving you, you're going to clear them out for me. Sound good?",
+            player: ">Do I have much of a choice in the matter?"
+        },
+        {
+            grogg: "Grogg: Well...'choice' is a subjective term. After each monster killed, you can choose to keep adventuring or choose to return and rest at my hut.",
+            player: ">But I can't leave. Is that right?"
+        },
+        {
+            grogg: '',
+            player: ''
+        }
     ];
     initialSelect.innerHTML = '';
     groggDialogue.textContent = introDialogue[dialogueIteration].grogg;
     playerResponse.textContent = introDialogue[dialogueIteration].player;
     initialSelect.append(groggDialogue, playerResponse);
-    playerResponse.addEventListener("click", function(){
+    playerResponse.addEventListener("click", function () {
         initialSelect.innerHTML = '';
         dialogueIteration++
         groggDialogue.textContent = introDialogue[dialogueIteration].grogg;
         playerResponse.textContent = introDialogue[dialogueIteration].player;
         initialSelect.append(groggDialogue, playerResponse);
-        if (dialogueIteration > 8){
+        if (dialogueIteration > 8) {
             letsGo();
             return;
         }
     });
 };
 
-function letsGo(){
+function letsGo() {
     groggDialogue.textContent = "Grogg: Not until your debt is paid, I'm afraid. It's not me, you see, it's the magical laws around debts and life's worth and...oh, nevermind. That's all terribly dull. Why don't we go back to my hut and you can head out in the morning, hm?";
     playerResponse.textContent = ">Sure, a rest would be nice [RETURN TO GROGG'S HUT]"
-    
-    playerResponse.addEventListener("click", function(){
+
+    playerResponse.addEventListener("click", function () {
         initialSelect.innerHTML = '';
         var adventureMsg = document.createElement("p");
         var letsgoBtn = document.createElement("button");
@@ -175,7 +199,7 @@ function letsGo(){
         adventureMsg.textContent = "After a full night's rest in Grogg's hut, you feel energized and ready to adventure..."
         adventureMsg.className = "dialogueBox";
         letsgoBtn.textContent = ">Alright, let's do this!"
-        letsgoBtn.addEventListener("click", function(){
+        letsgoBtn.addEventListener("click", function () {
             fightorRest();
         })
 
@@ -183,23 +207,23 @@ function letsGo(){
     })
 };
 
-function fightorRest(){
+function fightorRest() {
     initialSelect.innerHTML = '';
     var fightorrestPrompt = document.createElement("p");
     var fightBtn = document.createElement("button");
     var restBtn = document.createElement("button");
     fightorrestPrompt.className = "dialogueBox";
     fightorrestPrompt.textContent = "Wandering through the bog, you encounter a(n) [MONSTER NAME]. What do you do?";
-    
+
     fightBtn.textContent = ">Stay and fight--this will be easy!"
     fightBtn.className = "fightorrestBtn";
-    fightBtn.addEventListener("click", function(){
+    fightBtn.addEventListener("click", function () {
         console.log("Mama didn't raise a quitter");
         // place for monster generator function
     })
     restBtn.textContent = ">Flee and rest--I need to heal."
     fightBtn.className = "fightorrestBtn";
-    restBtn.addEventListener("click", function(){
+    restBtn.addEventListener("click", function () {
         console.log("Do I look like I wanna die rn?");
         // place for heal function
     })
